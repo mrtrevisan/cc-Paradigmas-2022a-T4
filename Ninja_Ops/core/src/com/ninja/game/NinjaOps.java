9b2a6c89b63049e83841c5657506c7a48c543d69
mrtrevisan <mauroroberto.trevisan@gmail.com>
2022-07-20 16:42:37 -0300

    basico

--- Ninja_Ops/core/src/com/ninja/game/NinjaOps.java
+++ Ninja_Ops/core/src/com/ninja/game/NinjaOps.java
@@ -1,31 +1,55 @@
 package com.ninja.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+
 
 public class NinjaOps extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	OrthographicCamera camera;
+	Player player;
+	Enemy enemy;
+	int detected;
 	
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		camera = new OrthographicCamera();
+      	camera.setToOrtho(false, 1920, 1080);		
+		player = new Player(0, 0, 500);
+		enemy = new Enemy(400, 400, 200); 
+		detected = 0;
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
+		camera.update();	
+		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(player.getImg(), player.getX(), player.getY());
+		batch.draw(enemy.getImg(), enemy.getX(), enemy.getY());
 		batch.end();
+
+		player.move();
+		GameUtils.camera_move(player, camera);
+		if (GameUtils.check_collision(player, enemy)) {
+			System.out.println("detected");
+		}
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		player.dispose();
+		enemy.dispose();
 	}
 }
