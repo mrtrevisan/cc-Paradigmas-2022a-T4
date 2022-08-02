2b7de33067d49fca0b5a2b28df7167b6ec0245bd
mrtrevisan <mauroroberto.trevisan@gmail.com>
2022-08-02 14:52:38 -0300

    sprites do player

--- Ninja_Ops/core/src/com/ninja/game/Player.java
+++ Ninja_Ops/core/src/com/ninja/game/Player.java
@@ -13,6 +13,8 @@ public class Player extends Rectangle{
         super(x, y, 0, 0);
         this.speed = speed;
         this.img_back = new Texture("player_back.png");
+        this.img_front = new Texture("player_front.png");
+        this.img_right = new Texture("player_right.png");
         this.img_left = new Texture("player_left.png");
         this.img_to_show = img_back;
         super.setWidth(img_to_show.getWidth());
@@ -28,13 +30,18 @@ public class Player extends Rectangle{
             super.x -= this.speed * Gdx.graphics.getDeltaTime();
             this.img_to_show = this.img_left;
         }
-      	if(Gdx.input.isKeyPressed(Keys.RIGHT)) super.x += this.speed * Gdx.graphics.getDeltaTime();
+      	if(Gdx.input.isKeyPressed(Keys.RIGHT)){
+            super.x += this.speed * Gdx.graphics.getDeltaTime();
+            this.img_to_show = this.img_right;
+        }
         if(Gdx.input.isKeyPressed(Keys.UP)){
             super.y += this.speed * Gdx.graphics.getDeltaTime();
             this.img_to_show = this.img_back;
         }
-		if(Gdx.input.isKeyPressed(Keys.DOWN)) super.y -= this.speed * Gdx.graphics.getDeltaTime();
-
+		if(Gdx.input.isKeyPressed(Keys.DOWN)){ 
+            super.y -= this.speed * Gdx.graphics.getDeltaTime();
+            this.img_to_show = this.img_front;
+        }
         if(super.x < 0) super.x = 0;
     	if(super.x > 1920 - super.width) super.x = 1920 - super.width;
 		if(super.y < 0) super.y = 0;
